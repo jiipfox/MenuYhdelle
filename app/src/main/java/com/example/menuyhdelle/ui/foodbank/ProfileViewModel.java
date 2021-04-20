852a79bce562138d0976ee73c80cd12f79688f8a
Aino Solin <73231276+ainosolin@users.noreply.github.com>
2021-04-20 16:11:07 +0300

    Omat tiedot fragment

---
+++ app/src/main/java/com/example/menuyhdelle/ui/foodbank/ProfileViewModel.java
@@ -0,0 +1,19 @@
+package com.example.menuyhdelle.ui.profile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Omat tiedot");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
