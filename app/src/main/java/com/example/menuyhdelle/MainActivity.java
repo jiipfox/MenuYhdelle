2b3518b8e6c1b5250aefc116dd097486bc3865f7
Jani Repo <jani.repo@student.lut.fi>
2021-04-25 14:07:12 +0300

    Json rest parser using volley. Example of usage in ReadXml "read theater area xml". Name should be changed at sdome point :)
    Also added volley dependency. Rebuild needed.

--- app/src/main/java/com/example/menuyhdelle/MainActivity.java
+++ app/src/main/java/com/example/menuyhdelle/MainActivity.java
@@ -1,33 +1,26 @@
 package com.example.menuyhdelle;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Menu;
 
 import com.example.menuyhdelle.ui.login.LoginActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                ReadXML.readTheatreAreaXML();
+                ReadXML.readTheatreAreaXML(getApplicationContext());
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
--- app/src/main/java/com/example/menuyhdelle/ReadXML.java
+++ app/src/main/java/com/example/menuyhdelle/ReadXML.java
@@ -1,5 +1,16 @@
 package com.example.menuyhdelle;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -17,7 +28,43 @@ public class ReadXML {
 
     private static ArrayList theatreArrayList;
 
-    static void readTheatreAreaXML(){
+    /**
+     * Read url rest json using from defined url using volley api. Put request on the queue and after
+     * response pick the Total CO2 and return. Handle exceptions todo.
+     *
+     * @param c context for the RequestQueue
+     */
+    static void readTheatreAreaXML(Context c){
+        RequestQueue queue = Volley.newRequestQueue(c);
+        String url ="https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/FoodCalculator?query.diet=omnivore&query.lowCarbonPreference=true&query.beefLevel=100&query.fishLevel=100&query.porkPoultryLevel=100&query.dairyLevel=100&query.cheeseLevel=100&query.riceLevel=100&query.eggLevel=100&query.winterSaladLevel=100&query.restaurantSpending=100";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject obj = null;
+                String referenceCo2Target = "";
+                try {
+                    obj = new JSONObject(response);
+                    int failure = obj.optInt("failure");
+                    System.out.println("JSON OBJ: " + obj.toString());
+                    referenceCo2Target = obj.getString("Total");
+                    System.out.println("Reference CO2 target = " + referenceCo2Target);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("ERROR");
+                }
+
+            });
+        queue.add(stringRequest);
+    }
+
+    static void readTheatreAreaXML1(){
 
         try{
             theatreArrayList = new ArrayList<String>();
